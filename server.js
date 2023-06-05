58d1556da9b67ec5a990ba8ff9844c1977a84149
Al-Kodach <alkodach351@gmail.com>
2023-06-05 07:31:31 -0400

    phase-1-com

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require("http");
+
+server = http.createServer((req, res) => {
+});
+const port = 5000;
+
+server.listen(port, () => {
+    console.log( "Successfully started the server on port 5000");
+});
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
